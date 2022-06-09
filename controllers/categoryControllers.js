aa90a6705d1e3443a96280d8e3c3b9f7018ac0f1
Andres Marquez <andresdmf55@gmail.com>
2022-06-09 13:14:46 -0500

    Updated categoryController

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -1,27 +1,28 @@
 const pool = require('../db/db');
 const categoryController = {};
 
+function queryResult(error, result, response){
+  if(error){
+    throw error;
+  } else{
+    response.status(200).json(result);
+    console.log('Categoria OK');
+  }
+}
+
 categoryController.categories = (req, res) => {
 
   pool.query('SELECT * FROM category', (error, result) => {
-    if(error){
-      throw error;
-    } else{
-      res.status(200).json(result);
-      console.log('Categoria OK');
-    }
+    
+    queryResult(error, result, res);
   });
 };
 
 categoryController.productsByCategory = (req, res) => {
 
   pool.query('SELECT * FROM product WHERE category=?', [req.params.id], (error, result) => {
-    if(error){
-      throw error;
-    } else {
-      res.status(200).json(result);
-      console.log('Product By Category OK');
-    }
+    
+    queryResult(error, result, res);
   });
 };
 

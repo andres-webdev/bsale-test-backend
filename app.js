f5f1f3460707a7fcea793c546f3e5b81b48faed1
Andres Marquez <andresdmf55@gmail.com>
2022-06-09 13:18:14 -0500

    Added new routes of products to main file

--- app.js
+++ app.js
@@ -6,11 +6,23 @@ const categoryRoutes = require('./routes/categoriesRoutes');
 
 app.use(cors());
 
-// Mostrar todos los productos
+// Mostrar todos los productos A-Z
 app.use(productRoutes.products);
 
 // Busca producto por nombre
 app.use(productRoutes.searchProduct);
+
+// Mostrar productos ordenados por nombre Z-A
+app.use(productRoutes.orderProductByNameDesc);
+
+// Mostrar los productos ordenados por precio - ASC
+app.use(productRoutes.orderProductByPriceAsc);
+
+// Mostrar los productos ordenados por precio - DESC
+app.use(productRoutes.orderProductByPriceDesc);
+
+// Mostrar productos con descuento
+app.use(productRoutes.discountProducts);
 
 // Lista de categorias
 app.use(categoryRoutes.category);

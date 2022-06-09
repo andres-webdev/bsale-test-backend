ed7dfa8821d4b0791b9e7f57925612d1af6e7831
Andres Marquez <andresdmf55@gmail.com>
2022-06-09 13:17:09 -0500

    Added new routes of products

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -6,6 +6,14 @@ const productRoutes = {};
 
 productRoutes.products = route.get('/api/products', productController.products);
 
-productRoutes.searchProduct = route.get('/api/products/:name', productController.productByName)
+productRoutes.searchProduct = route.get('/api/products/:name', productController.productByName);
+
+productRoutes.orderProductByNameDesc = route.get('/api/products/order/Z-A', productController.productsDesc);
+
+productRoutes.orderProductByPriceAsc = route.get('/api/products/order/lowerprice', productController.priceProductsAsc);
+
+productRoutes.orderProductByPriceDesc = route.get('/api/products/order/higherprice', productController.priceProductsDesc);
+
+productRoutes.discountProducts = route.get('/api/products/order/discount', productController.productsByDiscount);
 
 module.exports = productRoutes;
